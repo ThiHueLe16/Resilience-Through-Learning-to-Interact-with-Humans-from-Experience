e78827dbcf61aa7fb64bffdb8e1abe85143225c6
ThiHueLe16 <huethangnhat@gmail.com>
2025-11-26 00:58:09 +0100

    change in SEWrapperB1 to fix bug during training

--- BA_Thesis_Project/SafetyEnvelopeWrapperB1.py
+++ BA_Thesis_Project/SafetyEnvelopeWrapperB1.py
@@ -33,6 +33,8 @@ class SafetyEnvelopeWrapperB1(gym.Wrapper):
         # create a safe copy of original dict obs, info so that the wrapper can add new field but the underlying base env stay untouched.
         info = dict(info)
         info["safety_envelope_intervenes"] = self.safety_envelope_intervenes
+        initial_minimum_distance_Obstacles=self.compute_initial_d_min_to_obstacles(obs)
+        info['MINIMUM_OBSTACLE_DISTANCE']=initial_minimum_distance_Obstacles
         self.current_obs = obs
         self.current_info = info
         return self.current_obs, self.current_info
@@ -58,3 +60,32 @@ class SafetyEnvelopeWrapperB1(gym.Wrapper):
             terminated = True
         # set terminated to True when the safety envelope need to intervene to end the episode
         return self.current_obs, reward, terminated, truncated, self.current_info
+
+
+    @staticmethod
+    def compute_initial_d_min_to_obstacles(obs):
+        """
+            Return the initial minimum distance to all others obstacles. Need only for the begining, after 1 first step,
+            this distance is returned in the info after executing env.step(action)
+        """
+        d_min = float('inf')
+        # extract robot radius from the observation
+        robot_radius = obs["robot"][8]
+
+        # predict new distance from robot to each obstacle and find the minimum
+        for key in ["humans", "plants", "laptops", "tables", "walls"]:
+            if key not in obs:
+                continue
+            obstacle_obs = obs[key]
+            if obstacle_obs.size == 0:
+                continue
+            assert (len(obstacle_obs % 14 == 0))
+            number_of_obstacles = int(len(obstacle_obs) / 14)
+            for i in range(0, number_of_obstacles):
+                obstacle_relative_position_x = obstacle_obs[6 + i * 14]
+                obstacle_relative_position_y = obstacle_obs[7 + i * 14]
+                obstacle_radius = obstacle_obs[10 + i * 14]
+                distance= np.sqrt(obstacle_relative_position_x**2 + obstacle_relative_position_y**2 ) -robot_radius -obstacle_radius
+                if d_min > distance:
+                    d_min =distance
+        return d_min
